3da43fccbeab6fe787caf32aea9ec591f602fe41
Abavish <vnand@csu.fullerton.edu>
2018-11-03 22:00:35 -0700

    added callback for geocode

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -1,5 +1,6 @@
 const request = require('request')
-var geocodeAddress = (address) => {
+
+var geocodeAddress = (address, callback) => {
   var encodedAddress = encodeURIComponent(address)
   const APIkey = 'AWU07GqEPOYKIqgEGGbDcVNi3Df1SY9t'
   request({
@@ -7,14 +8,15 @@ var geocodeAddress = (address) => {
     json: true
   }, (error, response, body) => {
     if(error){
-      console.log('Unable to connect to server');
+      callback('Unable to connect to server')
+    }else{
+      callback(undefined, {
+        address: body.results[0].providedLocation.location,
+        latitiude: body.results[0].locations[0].latLng.lat,
+        longitude: body.results[0].locations[0].latLng.lng
+      })
     }
-    console.log(`Location is ${body.results[0].providedLocation.location}
-      Latitiude : ${body.results[0].locations[0].latLng.lat}
-      Longitude : ${body.results[0].locations[0].latLng.lng}`);
   })
 }
 
-module.exports = {
-  geocodeAddress
-}
+module.exports.geocodeAddress = geocodeAddress
